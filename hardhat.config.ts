c01164da8a8f6b1bd9055cd2833497e4517ec829
Andre <andreafragnelli@gmail.com>
2024-09-22 22:28:38 +0200

    inserito l'optimizer

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,11 +1,20 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-ignition";
 import * as dotenv from "dotenv";
 
 dotenv.config();
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.24",
+  solidity: {
+    version: "0.8.24",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
   networks: {
     sepolia: {
       url: process.env.SEPOLIA_RPC_URL || "",
