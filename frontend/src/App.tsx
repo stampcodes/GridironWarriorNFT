f132b883c0bf10a61819ca69fd05ceba5f663165
Andre <andreafragnelli@gmail.com>
2024-09-23 22:22:11 +0200

    utilizzo dell'hook per semplificare l'app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,32 @@
-import MintButton from "./components/MintButton";
+import RequestMint from "./components/RequestMint";
 import Navbar from "./components/Navbar";
 import NFTimg from "./components/NFTimg";
 
+import useMintRequest from "./hooks/useMintRequest";
+
 function App() {
+  const {
+    handleMintRequest,
+    mintRequestHash,
+    mintRequestError,
+    isMintRequestPending,
+    isMintRequestConfirming,
+    isMintRequestConfirmed,
+  } = useMintRequest();
+
   return (
     <>
       <div className="h-[100vh] w-[100%]">
         <Navbar />
         <NFTimg />
-        <MintButton />
+        <RequestMint
+          handleMintRequest={handleMintRequest}
+          mintRequestHash={mintRequestHash}
+          isMintRequestPending={isMintRequestPending}
+          isMintRequestConfirming={isMintRequestConfirming}
+          isMintRequestConfirmed={isMintRequestConfirmed}
+          mintRequestError={mintRequestError}
+        />
       </div>
     </>
   );
