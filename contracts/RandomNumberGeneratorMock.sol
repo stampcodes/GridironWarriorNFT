2466b233d5b8727a8420dd8f12e07dc0f0455293
Andre <andreafragnelli@gmail.com>
2024-10-01 11:40:58 +0200

    aggiornato il mock in conseguenza del cambiamento del contratto NFT.

--- contracts/RandomNumberGeneratorMock.sol
+++ contracts/RandomNumberGeneratorMock.sol
@@ -3,13 +3,28 @@ pragma solidity ^0.8.24;
 
 contract RandomNumberGeneratorMock {
     uint256 public lastRequestId = 0;
+    mapping(uint256 => bool) public requestFulfilled;
+    mapping(uint256 => uint256) public requestIdToRandomNumber;
 
     function requestRandomWords(bool) public returns (uint256) {
         lastRequestId++;
+        requestFulfilled[lastRequestId] = false;
+        requestIdToRandomNumber[lastRequestId] = 5;
         return lastRequestId;
     }
 
-    function getRequestStatus() public pure returns (bool, uint256) {
-        return (true, 5);
+    function fulfillRandomWords(uint256 _requestId) public {
+        require(_requestId <= lastRequestId, "Invalid request ID");
+        requestFulfilled[_requestId] = true;
+    }
+
+    function getRequestStatus(
+        uint256 _requestId
+    ) public view returns (bool, uint256) {
+        require(_requestId <= lastRequestId, "Invalid request ID");
+        return (
+            requestFulfilled[_requestId],
+            requestIdToRandomNumber[_requestId]
+        );
     }
 }
