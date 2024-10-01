5ace252f34990e90e6c7778ae127237b5b1b665d
Andre <andreafragnelli@gmail.com>
2024-10-01 12:42:01 +0200

    deploy

--- ignition/modules/RandomAndNFTModule.ts
+++ ignition/modules/RandomAndNFTModule.ts
@@ -1,15 +1,28 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 import parameters from "../../deploy/parameters";
 
+const existingRandomNumberGeneratorAddress =
+  "0x02fbB7cc9C164363Be267bB877801BC50aA78d1F";
+
 const RandomAndNFTModule = buildModule("RandomAndNFTModule", (m) => {
-  const randomNumberGenerator = m.contract("RandomNumberGenerator", [
-    parameters.subscriptionId,
-    parameters.keyHash,
-    parameters.callbackGasLimit,
-    parameters.requestConfirmations,
-    parameters.minRange,
-    parameters.maxRange,
-  ]);
+  // const randomNumberGenerator = existingRandomNumberGeneratorAddress
+  //   ? m.contractAt(
+  //       "RandomNumberGenerator",
+  //       existingRandomNumberGeneratorAddress
+  //     )
+  //   : m.contract("RandomNumberGenerator", [
+  //       parameters.subscriptionId,
+  //       parameters.keyHash,
+  //       parameters.callbackGasLimit,
+  //       parameters.requestConfirmations,
+  //       parameters.minRange,
+  //       parameters.maxRange,
+  //     ]);
+
+  const randomNumberGenerator = m.contractAt(
+    "RandomNumberGenerator",
+    existingRandomNumberGeneratorAddress
+  );
 
   const gridironWarriorNFT = m.contract("GridironWarriorNFT", [
     randomNumberGenerator,
